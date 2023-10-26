7b2e93cba2878e27a9f18f82ec36a78846afb932
Rajkumar <rajkumar.devaraveni@gmail.com>
2023-10-26 15:38:26 -0500

    Update actual_clean_up.groovy

--- actual_clean_up.groovy
+++ actual_clean_up.groovy
@@ -2,26 +2,28 @@ node('master') {
     def NODE_NAME = params.NODE_NAME ?: 'rdevara'
     echo "Node Name is: ${NODE_NAME}"
     
-    // Step 1: Take the Jenkins node offline
+    // Step 1: Disable the Jenkins node
     try {
         def node = Jenkins.instance.getNode(NODE_NAME)
         if (!node.toComputer().isOffline()) {
-            node.toComputer().disconnect()
-            echo "Node is offline for the Cleanup_Job."
+            node.toComputer().setTemporarilyOffline(true, null)
+            echo "Node is temporarily marked offline for the Cleanup_Job."
         } else {
             echo "Node is already offline for the Cleanup_Job."
         }
     } catch (Exception e) {
-        error "Failed to take the node offline for the Cleanup_Job: ${e.message}"
+        error "Failed to mark the node temporarily offline for the Cleanup_Job: ${e.message}"
     }
-    
-    // Step 2: Poll for running jobs
+
+    // Step 2: Poll for running builds and wait for them to finish
     def isIdle = false
     while (!isIdle) {
-        def runningJobs = Jenkins.instance.getItems(hudson.model.AbstractBuild.class)
-            .findAll { it.building && it.builtOnStr == NODE_NAME }
+        def runningBuilds = Jenkins.instance.getComputers().collectMany { it.executors }
+            .findAll { it.currentExecutable != null && it.name == NODE_NAME }
         
-        if (runningJobs.isEmpty()) {
+        echo "Running builds on ${NODE_NAME}: ${runningBuilds.size()}"
+        
+        if (runningBuilds.isEmpty()) {
             isIdle = true
         } else {
             echo "Waiting for builds to finish on ${NODE_NAME}..."
@@ -29,14 +31,20 @@ node('master') {
         }
     }
     
-    // Step 3: Bring the Jenkins node back online
+    if (isIdle) {
+        echo "No other builds are running on the node for the Cleanup_Job."
+    }
+    
+    sleep(time: 60, unit: 'SECONDS')
+    
+    // Step 3: Enable the Jenkins node to bring it back online
     try {
         def node = Jenkins.instance.getNode(NODE_NAME)
-        if (node.toComputer().isOffline()) {
-            node.toComputer().setAcceptingTasks(true)
+        if (node.toComputer().isTemporarilyOffline()) {
+            node.toComputer().setTemporarilyOffline(false, null)
             echo "Node is back online for the Cleanup_Job."
         } else {
-            echo "Node was already offline for the Cleanup_Job."
+            echo "Node was already temporarily offline for the Cleanup_Job."
         }
     } catch (Exception e) {
         error "Failed to bring the node back online for the Cleanup_Job: ${e.message}"
